23802776d4b8845458f033dbaeda7749b749d3dc
Etra-Games <73447562+Etra-Games@users.noreply.github.com>
2023-06-07 23:02:51 +0200

    Bridges adddeddd!!! YAaayyyy!

--- Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/Scripts/Editor/Tutorial Creator/LevelControllerInspector.cs
+++ Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/Scripts/Editor/Tutorial Creator/LevelControllerInspector.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static Etra.NonGamerTutorialCreator.Level.LevelController;
 
 namespace Etra.NonGamerTutorialCreator.Level.Editor
 {
     [CustomEditor(typeof(LevelController))]
     public class LevelControllerInspector : UnityEditor.Editor
     {
+        SerializedProperty bridgeOptions;
+        LevelController script;
+        private void OnEnable()
+        {
+            // hook up the serialized properties
+            bridgeOptions = serializedObject.FindProperty(nameof(LevelController.bridgeOptions));
+        }
+
         public override void OnInspectorGUI()
         {
-            var script = (LevelController)target;
+            DrawDefaultInspector();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(bridgeOptions);
+            serializedObject.ApplyModifiedProperties();
+            LevelController script = (LevelController)target;
+
+
 
             if (script.isPreview)
                 EditorGUILayout.HelpBox("This object is temporary. Create it by finishing the Tutorial Creator wizard.", MessageType.Info);
@@ -20,9 +36,11 @@ namespace Etra.NonGamerTutorialCreator.Level.Editor
                 EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(LevelController.chunks)));
 
                 GUILayout.Space(EditorGUIUtility.singleLineHeight);
-                if (GUILayout.Button("Edit", GUILayout.Height(32f))) //TODO: make this work
-                { }
             }
+
+            if (GUILayout.Button("Apply Changes", GUILayout.Height(32f))) 
+            { script.ResetAllChunksPositions(); }
+
         }
     }
 }
--- Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/Scripts/Runtime/Level/LevelController.cs
+++ Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/Scripts/Runtime/Level/LevelController.cs
@@ -1,26 +1,100 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Etra.NonGamerTutorialCreator.Level
 {
     public class LevelController : MonoBehaviour
     {
-        public bool isPreview;
+        string flatBridgeName = "FlatBridgeChunk";
+        string angledBridgeName = "AngledBridgeChunk";
+        public enum BridgeOptions
+        {
+            NoBridges,
+            FlatBridges,
+            AngledBridges
+        }
 
+        [HideInInspector]
+        public BridgeOptions bridgeOptions = BridgeOptions.AngledBridges;
+
+        [HideInInspector]
+        public bool isPreview;
+        [HideInInspector]
         public List<LevelChunkObject> chunks = new List<LevelChunkObject>();
 
-        public bool ooooga;
 
         /// <summary>Forces all chunks to connect properly</summary>
+        /// 
+
+
         public void ResetAllChunksPositions()
         {
+            destroyBridgesImmediate();
+            chooseBridgeOption();
+        }
+
+        void chooseBridgeOption()
+        {
+            switch (bridgeOptions)
+            {
+                case BridgeOptions.NoBridges:
+                    placeChunks(chunks);
+                    break;
+
+                case BridgeOptions.FlatBridges:
+                    List<LevelChunkObject> chunksPlusBridges = addBridges(chunks, flatBridgeName);
+                    placeChunks(chunksPlusBridges);
+                    break;
+
+                case BridgeOptions.AngledBridges:
+                    List<LevelChunkObject> chunksPlusBridges1 = addBridges(chunks, angledBridgeName);
+                    placeChunks(chunksPlusBridges1);
+                    break;
+            }
+        }
+
+        void placeChunks(List<LevelChunkObject> chunkList)
+        {
             Vector3 offset = Vector3.zero;
-            foreach (var chunk in chunks)
+            foreach (var chunk in chunkList)
             {
                 offset -= chunk.startConnectionPoint;
                 chunk.transform.localPosition = offset;
                 offset += chunk.endConnectionPoint;
             }
         }
+
+        List<LevelChunkObject> addBridges(List<LevelChunkObject> initialChunks, string bridgeName)
+        {
+            List<LevelChunkObject> returnedChunksPlusBridges = new List<LevelChunkObject>();
+
+            for (int i = 0; i < initialChunks.Count; i++)
+            {
+                GameObject loadedBridge = Resources.Load<GameObject>(bridgeName);
+
+                returnedChunksPlusBridges.Add(initialChunks[i]); // add initial chunk
+                if (i != chunks.Count - 1)
+                {
+                    GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(loadedBridge, this.transform);
+                    prefab.name = bridgeName;
+                    returnedChunksPlusBridges.Add(prefab.GetComponent<LevelChunkObject>());
+                }
+            }
+
+            return returnedChunksPlusBridges;
+
+        }
+
+        void destroyBridgesImmediate()
+        {
+            BridgeIdentifier[] myItems = FindObjectsOfType(typeof(BridgeIdentifier)) as BridgeIdentifier[];
+            foreach (BridgeIdentifier item in myItems)
+            {
+                DestroyImmediate(item.gameObject);
+            }
+        }
+
+
     }
 }
