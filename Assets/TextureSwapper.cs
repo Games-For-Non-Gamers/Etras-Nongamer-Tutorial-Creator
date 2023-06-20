271500d3202c607f1a079fa945fe3d929210bb05
Etra-Games <73447562+Etra-Games@users.noreply.github.com>
2023-06-21 01:44:15 +0200

    reordered mats

--- Assets/TextureSwapper.cs
+++ Assets/TextureSwapper.cs
@@ -6,105 +6,114 @@ using UnityEngine;
 
 public class TextureSwapper : MonoBehaviour
 {
-
     private string materialPath = "SwappableMaterials"; // The folder path where the textures are located
     private Material[] materialArray;
-
 
     private string texturePath = "SwappableTextures/"; // The folder path where the materials are located
     private Texture2D[] textureArray;
 
-    public string[] packs = { "Winthor64", "Acme128", "Cubed128", "Firewolf128", "Invictus64", "Pixels16" };
-    public int selected = 0;
+    public enum TexturePack
+    {
+        [InspectorName("(Default)-Winthor64x")]
+        Winthor64,
+        [InspectorName("(Realistic)-Cubed128x")]
+        Cubed128,
+        [InspectorName("(Simple)-Firewolf128x")]
+        Firewolf128,
+        [InspectorName("(Pixel)-Pixels16x")]
+        Pixels16,
+        [InspectorName("(Toon)-Acme128x")]
+        Acme128,
+        [InspectorName("(Stylized)-Invictus64x")]
+        Invictus64,
+        Custom // Added custom option
+    }
 
+    public TexturePack selectedTexturePack = TexturePack.Winthor64;
+    private string customTexturePackName = ""; // Added custom string input
+    public bool normalMapsEnabled = true;
     public FilterMode filterMode = FilterMode.Bilinear;
 
-    [ContextMenu("SwapTextures")]
+    private void OnValidate()
+    {
+        // Perform validation logic here if needed
+    }
+
     public void SwapTextures()
     {
-        string addedString = packs[selected];
+        string addedString;
+        if (selectedTexturePack == TexturePack.Custom && !string.IsNullOrEmpty(customTexturePackName))
+        {
+            addedString = customTexturePackName;
+        }
+        else
+        {
+            addedString = selectedTexturePack.ToString();
+        }
+
         string moddedTexturePath = texturePath + addedString;
         Debug.Log("Path" + texturePath);
         materialArray = Resources.LoadAll<Material>(materialPath);
         textureArray = Resources.LoadAll<Texture2D>(moddedTexturePath);
 
         List<textureSwapData> swapData = new List<textureSwapData>();
-        foreach (Material material in materialArray)
+        for (int i = 0; i < materialArray.Length; i++)
         {
+            Material material = materialArray[i];
             swapData.Add(new textureSwapData(material.name, false));
         }
 
-        //Material material in materialArray
-        foreach (Texture2D texture in textureArray)
+        for (int i = 0; i < textureArray.Length; i++)
         {
+            Texture2D texture = textureArray[i];
             texture.filterMode = filterMode;
-            foreach (Material material in materialArray)
+
+            for (int j = 0; j < materialArray.Length; j++)
             {
-                Material newMaterial = new Material(material);
-                //Swap albedo
-                if (newMaterial.mainTexture != null)
+                Material material = materialArray[j];
+
+                // Swap albedo
+                if (material.mainTexture != null)
                 {
-                    Texture2D newTexture = newMaterial.mainTexture as Texture2D;
+                    Texture2D newTexture = material.mainTexture as Texture2D;
 
-                    if (newTexture.name == texture.name )
+                    if (newTexture.name == texture.name)
                     {
-                        newMaterial.mainTexture = texture;
-                        
-                    }
-
-                }
-
-                //Swap normal
-                if (newMaterial.GetTexture("_BumpMap") != null)
-                {
-                    Texture2D normalTexture = newMaterial.GetTexture("_BumpMap") as Texture2D;
-                    //If matches swap and turn on normal
-                    if(normalTexture.name == texture.name)
-                    {
-                        newMaterial.SetTexture("_BumpMap", normalTexture);
-                        newMaterial.EnableKeyword("_NORMALMAP");
-
-                        textureSwapData data = swapData.Find(swap => swap.textureName == newMaterial.name);
-                        if (data != null)
-                        {
-                            data.normalSwapped = true;
-                            EditorUtility.SetDirty(newMaterial);
-                        }
-
+                        material.mainTexture = texture;
                     }
                 }
-            }
 
-        }
-
-
-        List<Material> materialList = materialArray.ToList();
-
-        //Go through each mat. If swap happened do nothing. If no swap, set bump map int to 0
-        foreach (textureSwapData swapD in swapData)
-        {
-            if (swapD.normalSwapped == false)
-            {
-                Material mat = materialList.Find(x => x.name == swapD.textureName);
-                Material newMaterial = new Material(mat);
-                if (newMaterial != null)
+                // Swap normal
+                if (material.GetTexture("_BumpMap") != null)
                 {
-                    if (newMaterial.GetTexture("_BumpMap") != null)
+
+                    if (normalMapsEnabled)
                     {
-                        newMaterial.DisableKeyword("_NORMALMAP");
-                        EditorUtility.SetDirty(newMaterial);
+                        material.EnableKeyword("_NORMALMAP");
                     }
-                        
+                    else
+                    {
+                        material.DisableKeyword("_NORMALMAP");
+                    }
                 }
+
+                EditorUtility.SetDirty(material);
             }
         }
 
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
 
+        // Update material inspectors to reflect the changes
+        foreach (Material material in materialArray)
+        {
+            EditorUtility.SetDirty(material);
+            AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(material));
+        }
     }
 
-    internal class textureSwapData{
+    internal class textureSwapData
+    {
         public string textureName = "";
         public bool normalSwapped = false;
 
@@ -113,7 +122,29 @@ public class TextureSwapper : MonoBehaviour
             textureName = n;
             normalSwapped = s;
         }
-
     }
 
+    // Add the following method to display the inspector button and custom string field
+#if UNITY_EDITOR
+    [CustomEditor(typeof(TextureSwapper))]
+    public class TextureSwapperEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            TextureSwapper textureSwapper = (TextureSwapper)target;
+
+            if (textureSwapper.selectedTexturePack == TextureSwapper.TexturePack.Custom)
+            {
+                textureSwapper.customTexturePackName = EditorGUILayout.TextField("Custom Texture Pack Name", textureSwapper.customTexturePackName);
+            }
+
+            if (GUILayout.Button("Swap Textures"))
+            {
+                textureSwapper.SwapTextures();
+            }
+        }
+    }
+#endif
 }
