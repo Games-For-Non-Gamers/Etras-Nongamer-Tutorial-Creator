f8017d7a8402912bbea9b039a552bca2b1b7fe4b
Etra-Games <73447562+Etra-Games@users.noreply.github.com>
2023-06-21 21:33:59 +0200

    added crest. GEtting two errors. Gonna experiment in other branch

---
+++ Assets/crest/Assets/Development/Scripts/TestCameraChange.cs
@@ -0,0 +1,75 @@
+﻿// Crest Ocean System
+
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+#if CREST_UNITY_INPUT && ENABLE_INPUT_SYSTEM
+#define INPUT_SYSTEM_ENABLED
+#endif
+
+using System.Collections.Generic;
+using UnityEngine;
+
+#if INPUT_SYSTEM_ENABLED
+using UnityEngine.InputSystem;
+#endif
+using UnityEngine.SceneManagement;
+
+namespace Crest
+{
+    public class TestCameraChange : MonoBehaviour
+    {
+        [Tooltip("This will populate on start if left empty. Use 'C' to cycle cameras.")]
+        [SerializeField] List<Camera> _cameras = new List<Camera>();
+
+        void OnEnable()
+        {
+            if (_cameras.Count > 0)
+            {
+                return;
+            }
+
+            // We want all active and inactive cameras. Yes, this appears to be the best way to do this...
+            foreach (var gameObject in SceneManager.GetActiveScene().GetRootGameObjects())
+            {
+                foreach (var camera in gameObject.GetComponentsInChildren<Camera>(true))
+                {
+                    if (camera.hideFlags == HideFlags.NotEditable || camera.hideFlags == HideFlags.HideAndDontSave)
+                    {
+                        continue;
+                    }
+
+                    _cameras.Add(camera);
+                }
+            }
+        }
+
+        void Update()
+        {
+            // New input system works even when game view is not focused.
+            if (!Application.isFocused)
+            {
+                return;
+            }
+
+#if INPUT_SYSTEM_ENABLED
+            if (Keyboard.current.cKey.wasReleasedThisFrame)
+#else
+            if (Input.GetKeyUp(KeyCode.C))
+#endif
+            {
+                // Cycle camera
+                Camera previous = _cameras[_cameras.Count - 1];
+                foreach (var current in _cameras)
+                {
+                    if (previous.gameObject.activeInHierarchy)
+                    {
+                        previous.gameObject.SetActive(false);
+                        current.gameObject.SetActive(true);
+                        break;
+                    }
+                    previous = current;
+                }
+            }
+        }
+    }
+}
