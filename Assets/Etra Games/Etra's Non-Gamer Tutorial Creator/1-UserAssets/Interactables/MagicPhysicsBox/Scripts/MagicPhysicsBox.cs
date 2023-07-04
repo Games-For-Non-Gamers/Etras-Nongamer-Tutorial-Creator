87a783d8881130f7d6d9fe89726585602077de03
Etra-Games <73447562+Etra-Games@users.noreply.github.com>
2023-07-04 14:58:22 +0200

    about to update namespaces

--- Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/1-UserAssets/Interactables/MagicPhysicsBox/Scripts/MagicPhysicsBox.cs
+++ Assets/Etra Games/Etra's Non-Gamer Tutorial Creator/1-UserAssets/Interactables/MagicPhysicsBox/Scripts/MagicPhysicsBox.cs
@@ -4,56 +4,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicPhysicsBox : MonoBehaviour
+
+namespace Etra.NonGamerTutorialCreator
 {
-    Vector3 startLocation;
-    public bool hasSetRange = false;
-    public Vector2 xRange = new Vector2(30, -30);
-    public Vector2 zRange = new Vector2(30, -30);
-    void Start()
+    public class MagicPhysicsBox : MonoBehaviour
     {
-        startLocation = transform.position;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<CheckpointTeleporter>())
+        Vector3 startLocation;
+        public bool hasSetRange = false;
+        public Vector2 xRange = new Vector2(30, -30);
+        public Vector2 zRange = new Vector2(30, -30);
+        void Start()
         {
-            respawn();
+            startLocation = transform.position;
         }
-    }
 
-    void respawn()
-    {
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-        transform.position = startLocation;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (hasSetRange)
+        private void OnTriggerEnter(Collider other)
         {
-            //When this object lands check if it is within range, if not, teleport to start
-            if (transform.position.x > startLocation.x  + xRange.x )
+            if (other.GetComponent<CheckpointTeleporter>())
             {
                 respawn();
             }
+        }
 
-            if (transform.position.x < startLocation.x + xRange.y)
+        void respawn()
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.position = startLocation;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (hasSetRange)
             {
-                respawn();
-            }
+                //When this object lands check if it is within range, if not, teleport to start
+                if (transform.position.x > startLocation.x + xRange.x)
+                {
+                    respawn();
+                }
 
-            if (transform.position.z > startLocation.z + zRange.x)
-            {
-                respawn();
-            }
+                if (transform.position.x < startLocation.x + xRange.y)
+                {
+                    respawn();
+                }
 
-            if (transform.position.z < startLocation.z + zRange.y)
-            {
-                respawn();
-            }
+                if (transform.position.z > startLocation.z + zRange.x)
+                {
+                    respawn();
+                }
 
+                if (transform.position.z < startLocation.z + zRange.y)
+                {
+                    respawn();
+                }
+
+            }
         }
     }
 }
